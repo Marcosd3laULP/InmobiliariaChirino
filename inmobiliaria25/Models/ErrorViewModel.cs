256b446736ac70a9dcd2b0fd901810cff583dccd
Marcosd3laULP <usuario4ltnum1@gmail.com>
2025-08-19 15:47:30 -0300

    Iniciamos Papu
    Ya vamos a armar bien sabroso este proyecto

---
+++ inmobiliaria25/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace inmobiliaria25.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
