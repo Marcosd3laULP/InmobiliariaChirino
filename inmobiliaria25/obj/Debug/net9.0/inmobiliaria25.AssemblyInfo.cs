b13978e33fa9976e11335e1762cc10b339a5001b
MarcosBR--ULP2024🦈(☞ﾟヮﾟ)☞☜(ﾟヮﾟ☜) <usuario4ltnum1@gmail.com>
2025-08-25 19:31:21 -0300

    abm de propietario y bd normalizada
    Ya todas las operaciones de la entidad propietarios estan listas, la base de datos tiene un usuario y contraseña de tipo "Universal" para evitar problemas de conexion al desarrollar el proyecto en otra computadora.

--- inmobiliaria25/obj/Debug/net9.0/inmobiliaria25.AssemblyInfo.cs
+++ inmobiliaria25/obj/Debug/net9.0/inmobiliaria25.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("inmobiliaria25")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ee978d3b4df3c954fe0767620ea168aee87891d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8f5a2e56e767fbcc7b0d3430acc1a7be1c617c")]
 [assembly: System.Reflection.AssemblyProductAttribute("inmobiliaria25")]
 [assembly: System.Reflection.AssemblyTitleAttribute("inmobiliaria25")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
